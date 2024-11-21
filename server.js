536e7fdb7de7d62ae52d686c3e1d39dd2bc7b945
MarcosOki <marcosokita181@gmail.com>
2024-11-21 18:07:37 -0300

    Aula 2

--- server.js
+++ server.js
@@ -1,13 +1,52 @@
 import express from "express"
 
-
+const posts = [
+    {
+        id:1,
+        descricao: "uma foto teste",
+        url: "https://placecats.com/millie/300/150"
+    },
+    {
+        id:2,
+        descricao: "Uma foto linda de um gatinho",
+        url: "https://placecats.com/millie/300/150"
+    },
+    {
+        id:3,
+        descricao: "Um gatinho fazendo careta",
+        url: "https://placecats.com/millie/300/150"
+    },
+    {
+        id:4,
+        descricao: "Gato preguiçoso tomando sol",
+        url: "https://placecats.com/millie/300/150"
+    },
+    {
+        id:5,
+        descricao: "Gato explorando a caixa",
+        url: "https://placecats.com/millie/300/150"
+    },
+    {
+        id:6,
+        descricao: "Gato comendo ração",
+        url: "https://placecats.com/millie/300/150"
+    }
+]
 
 const app = express()
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log("Listening on port 3000")
 })
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Hello from the server")
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts)
+})
+function buscarPostsPorId(id){
+    return posts.findIndex((post)=>{return post.id === Number(id)})
+}
+app.get("/posts/:id", (req,res) => {
+    const index = buscarPostsPorId(req.params.id)
+    return res.status(200).json(posts[index])
 })
